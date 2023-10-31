f3b9f4eaa7f203c7e3030d43ccab673bf9c2ea61
javoh1rkhon <rustamovjavohirkhon@gmail.com>
2023-10-31 15:07:09 +0500

    1-dars dom

---
+++ main.js
@@ -0,0 +1,24 @@
+// DOM === Document object module
+
+// let h1 = document.getElementsByTagName('h1')
+// let h1class = document.getElementsByClassName('genrix')
+// let h1Id = document.getElementById('salom')
+
+// let a = document.querySelector('#salom')
+// let b = document.querySelector('.genrix')
+// let d = document.querySelector('h1')
+
+// let f = document.querySelectorAll('h1')
+
+// console.log(f);
+// console.log(h1class);
+
+let a = document.querySelector('.text')
+let box = document.querySelector('.box')
+// aa.innerText = 'Hello'
+
+a.innerHTML = '<span>hi</span>'
+
+box.style.width = '200px'
+box.style.height = '200px'
+box.style.backgroundColor = 'red'
